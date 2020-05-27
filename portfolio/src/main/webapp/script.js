cdb42ba1a54af5adf28ba00d885c803b2991870f
Nico <nicolas.pv.et@hotmail.com>
2020-05-27 15:11:27 -0600

    remove greeting

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,20 +13,8 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Displays random fact about me
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
 function addRandomFact() {
   var num = 100;
   const facts =
