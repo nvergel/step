ef8b93f615806a42fb50d542cf17bb141712458b
Nico <nicolas.pv.et@hotmail.com>
2020-05-27 17:10:16 -0600

    move bonzai pic to fun facts section and add orhid pic

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,6 +36,31 @@ function addRandomFact() {
         }, 20);
 }
 
+function hideUnhide(content)  {
+    if (content.style.display === "block") {
+            content.style.display = "none";
+            } else {
+            content.style.display = "block";
+            }
+}
+
+function collabsible() {
+    var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+            content.style.display = "none";
+            } else {
+            content.style.display = "block";
+            }
+        });
+        }
+}
+
 function dispM1() {
 
     document.getElementById('m1').style.display = "block"; 
